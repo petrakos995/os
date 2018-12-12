b1131e5b4e1ef4c4351bd1454b68faabbc2e39a7
1191233440 <petra.kos@fer.hr>
2018-12-12 17:47:00 +0100

    treci labos

--- treci_labos/Nfilozofa.c
+++ treci_labos/Nfilozofa.c
@@ -13,6 +13,7 @@ pthread_mutex_t M;
 pthread_cond_t CONDITIONS;
 int broj_filozofa;
 int broj_stapica;
+int polje[100];
 
 void zauzmi(int id){
 	broj_stapica -= id;
@@ -65,22 +66,37 @@ void *proces_dretva(void *x){
 
 int main(void){
 
+	pthread_t filozofi[100];
+
 	printf("prije otvaranja dat\n");
 	FILE *text;
-    text = fopen("/home/petra/Desktop/os/treci_labos/dat.txt", "r");
+    text = fopen("dat.txt", "r+");
 
     printf("poslije otvaranja dat\n");
-    fscanf(text, "%lf %lf", &broj_stapica, &broj_filozofa);
+    int brojac = 0;
 
-    int ruke; 
-    pthread_t filozofi[broj_filozofa];
+    //fscanf(text, "%d %d", &broj_stapica, &broj_filozofa);
+    while(fscanf(text, "%d", &polje[brojac]) !=EOF){
+    	brojac++;
+    }
+
+    broj_stapica = polje[0];
+    broj_filozofa = polje[1];
+	
+    if (pthread_mutex_init(&M, NULL) != 0)
+    {
+        printf("\n Monitor se nije uspio inicijalizirati\n");
+        return 1;
+    }
+    if(pthread_cond_init(&CONDITIONS,NULL)!=0) {
+    	printf("iniciajliziranje reda uvjeta trgova nije uspjelo\n");
+    	return 1;
+    }
 
     printf("prije for 1 %d %d \n", broj_stapica, broj_filozofa);
 
     for(int i = 0; i < broj_filozofa; ++i) {
-    	fscanf(text, "%lf",&ruke);
-    	printf("%d\n", ruke);
-       	if (pthread_create(filozofi+i, NULL, proces_dretva, &ruke)) {
+       	if (pthread_create(filozofi+i, NULL, proces_dretva, &polje[i+2])) {
 			printf("Greska pri stvaranju dretve\n");
 			exit(1);
 		} 
